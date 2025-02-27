7119dbb2452b6053afd6247368111509235e5183
Kunal-Rawat-TTN <kunal.rawat@tothenew.com>
2025-02-27 22:35:18 +0530

    Question_4

--- src/test/java/com/kunalTTN/MainTest.java
+++ src/test/java/com/kunalTTN/MainTest.java
@@ -3,8 +3,6 @@ package com.kunalTTN;
 import junit.framework.TestCase;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class MainTest extends TestCase {
     @Test
     public void testDemo()
