84696093a85ba75afda0d1505afb483242eaac36
Kunal-Rawat-TTN <kunal.rawat@tothenew.com>
2025-02-27 16:37:16 +0530

    Question_1&2

---
+++ src/test/java/com/kunalTTN/MainTest.java
@@ -0,0 +1,14 @@
+package com.kunalTTN;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest extends TestCase {
+    @Test
+    public void testDemo()
+    {
+        assertEquals(12,12);
+    }
+}
